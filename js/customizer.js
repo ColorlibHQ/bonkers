02d6f437384a46704b750c05fd673dd20db84536
Giucu <giucu91@yahoo.com>
2017-10-04 14:09:25 +0300

    #6

--- js/customizer.js
+++ js/customizer.js
@@ -1,14 +1,6 @@
 jQuery(window).load(function (){
 	( function( $ ) {
 
-		/*
-		 * Links to different sections in the Customizer
-		 * Just create a link like this: <a href="#" data-section="section-id">link</a>
-		 */
-		$('body').on('click', 'a[data-section]', function(event) {
-			wp.customize.section( $(this).attr( 'data-section' ) ).focus();
-		});
-
 
 		/*
 		 * Scroll to show the current section
